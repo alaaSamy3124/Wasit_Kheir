247e9bd29a15e6a5a423e7afe5929edbfd2f5ad6
marwarafat <mrz456058@gmail.com>
2026-05-06 20:40:37 +0300

    delete files

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:wasit_kheir/settings/view/settings_screen.dart';
-import 'package:wasit_kheir/settings/view/widgets/change_password.dart';
-import 'package:wasit_kheir/settings/view/widgets/contact_us.dart';
-import 'package:wasit_kheir/settings/view/widgets/edit_profile.dart';
-import 'package:wasit_kheir/settings/view/widgets/privacy_policy.dart';
-import 'package:wasit_kheir/settings/view/widgets/report_abuse.dart';
-import 'package:wasit_kheir/settings/view/widgets/teams_conditions.dart';
+import 'package:wasit_kheir/chat/view/chat_screen.dart';
+import 'package:wasit_kheir/chat/view/widgets/empty_chat.dart';
+// import 'package:wasit_kheir/notification/view/notifications_screen.dart';
+// import 'package:wasit_kheir/profile/view/profile_screen.dart';
+// import 'package:wasit_kheir/report/report_item_view.dart';
+// import 'package:wasit_kheir/settings/view/settings_screen.dart';
+// import 'package:wasit_kheir/settings/view/widgets/change_password.dart';
+// import 'package:wasit_kheir/settings/view/widgets/contact_us.dart';
+// import 'package:wasit_kheir/settings/view/widgets/edit_profile.dart';
+// import 'package:wasit_kheir/settings/view/widgets/privacy_policy.dart';
+// import 'package:wasit_kheir/settings/view/widgets/report_abuse.dart';
+// import 'package:wasit_kheir/settings/view/widgets/teams_conditions.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,17 +23,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/settings',
+      // initialRoute: '/profile',
 
-      routes: {
-        '/settings': (context) => const SettingsView(),
-        '/profile': (context) => const ProfileInformationScreen(),
-        '/changePassword': (context) => const ChangePasswordScreen(),
-        '/contact': (context) => const ContactUsScreen(),
-        '/privacy': (context) => const PrivacyPolicy(),
-        '/terms': (context) => const TermsConditions(),
-        '/report': (context) => const ReportAbuse(),
-      },
+      // routes: {
+      //   '/settings': (context) => const SettingsView(),
+      //   '/profile': (context) => const ProfileInformationScreen(),
+      //   '/changePassword': (context) => const ChangePasswordScreen(),
+      //   '/contact': (context) => const ContactUsScreen(),
+      //   '/privacy': (context) => const PrivacyPolicy(),
+      //   '/terms': (context) => const TermsConditions(),
+      //   '/report': (context) => const ReportAbuse(),
+      // },
+      home: EmptyChatScreen(),
+      //  ChatScreen(),
     );
   }
 }
